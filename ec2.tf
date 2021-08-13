b628fb3042089a714b753062d1de81601d85ed21
kishore <hsk@gmail.com>
2021-08-13 22:29:41 +0530

    ec2.tf file added

---
+++ ec2.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "public-instance" {
+    for_each = toset(data.aws_availability_zones.azs.names)
+    ami = var.imagename
+    instance_type = "t2.micro"
+    key_name = "${var.key_name}"
+    subnet_id = "${element(aws_subnet.public-subnet.*.id, index(data.aws_availability_zones.azs.names,each.value))}"
+    vpc_security_group_ids = [ "${aws_security_group.allow_all.id}" ]
+    associate_public_ip_address = true
+    tags = {
+        Name = "Public-Server-${each.value}"
+    }
+}
